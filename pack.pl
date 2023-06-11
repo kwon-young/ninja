0a9fb19fb239cae99b7997e945a5061f3fe76da9
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2023-06-11 21:12:33 +0200

    update pack name

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(ninja).
 title('Ninja build system generator').
-version('0.1').
+version('0.2').
 author('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 maintainer('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 packager('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
-home('https://github.com/kwon-young/ninja.pl').
-download('https://github.com/kwon-young/ninja.pl/releases/*.zip').
+home('https://github.com/kwon-young/ninja').
+download('https://github.com/kwon-young/ninja/releases/*.zip').
 provides(ninja).
