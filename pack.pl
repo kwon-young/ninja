241dc93e7334f361e918949155340c755dd6d613
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2023-06-11 19:57:46 +0200

    add a download url.

--- pack.pl
+++ pack.pl
@@ -5,4 +5,5 @@ author('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 maintainer('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 packager('Kwon-Young Choi', 'kwon-young.choi@hotmail.fr').
 home('https://github.com/kwon-young/ninja.pl').
+download('https://github.com/kwon-young/ninja.pl/releases/*.zip').
 provides(ninja).
